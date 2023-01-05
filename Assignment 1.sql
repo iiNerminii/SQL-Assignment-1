f44b93ddf0c27c35d31f84cf2ee63ea3203a73e7
Nermin Muratovic <115759685+iiNerminii@users.noreply.github.com>
2023-01-05 13:23:11 -0600

    Add files via upload

---
+++ Assignment 1.sql
@@ -0,0 +1,98 @@
+-- 1. How many actors are there with the last name ‘Wahlberg’?
+select *
+from actor 
+
+select count(last_name)
+from actor 
+where last_name = 'Wahlberg';
+
+-- 2. How many payments were made between $3.99 and $5.99?
+select * 
+from payment
+
+select count(amount)
+from payment 
+where amount between 3.99 and 5.99
+
+-- 3. What film does the store have the most of? (search in inventory)
+select *
+from inventory 
+
+select film_id, count(film_id)
+from inventory 
+group by film_id
+order by count(film_id) desc  
+
+-- 4. How many customers have the last name ‘William’?
+select *
+from customer 
+
+select count(last_name)
+from customer 
+where last_name like 'William';
+
+-- 5. What store employee (get the id) sold the most rentals?
+select *
+from rental 
+
+select staff_id, count(staff_id)
+from rental 
+group by staff_id
+order by count(staff_id) desc  
+
+-- 6. How many different district names are there?
+select *
+from address 
+
+select count(distinct district)
+from address 
+
+-- 7. What film has the most actors in it? (use film_actor table and get film_id)
+select *
+from film_actor
+
+select film_id, count(actor_id) 
+from film_actor 
+group by film_id
+order by count(actor_id) desc  
+
+-- 8. From store_id 1, how many customers have a last name ending with ‘es’? (use customer table)
+select * 
+from customer
+
+select store_id , count(last_name) 
+from customer 
+where last_name like '%es'
+group by store_id 
+
+-- 9. How many payment amounts (4.99, 5.99, etc.) had a number of rentals above 250 for customers with ids between 380 and 430? (use group by and having > 250)
+select * 
+from payment 
+
+select amount, count(amount) 
+from payment 
+where customer_id between 380 and 430 
+group by amount 
+order by count(rental_id) desc
+
+-- 10. Within the film table, how many rating categories are there? And what rating has the most movies total?
+select *
+from film
+
+select count(distinct rating) 
+from film
+
+select rating, count(film_id), count(distinct rating)
+from film
+group by rating 
+order by count(film_id) desc 
+
+
+
+
+
+
+
+
+
+
